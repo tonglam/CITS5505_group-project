4fcaf5c94da2414826dbea1d63ee3f50f1007e05
enoch2022new <100208118+enoch2022new@users.noreply.github.com>
2024-03-25 20:22:32 +0800

    Flask-migrate done

--- app/extensions.py
+++ app/extensions.py
@@ -3,13 +3,11 @@
 from flask_apscheduler import APScheduler
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from app.constant import FlashAlertTypeEnum
 
 db = SQLAlchemy()
-migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.login_message = "Please log in."
