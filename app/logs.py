cc4b99d10965929f206da46ecfeda5de19baee9b
enoch2022new <100208118+enoch2022new@users.noreply.github.com>
2024-04-06 09:15:58 +0800

    logs record by date

--- app/logs.py
+++ app/logs.py
@@ -1,6 +1,6 @@
 """log config"""
 
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 import logging
 import os
 
@@ -9,12 +9,11 @@ def configure_logging(app):
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/application.log', maxBytes=102400,
-                                       backupCount=10000000)
+    file_handler = TimedRotatingFileHandler('logs/application.log', when='D', interval=1, backupCount=30)
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(file_handler)
+    app.logger.addHandler(file_handler``)
 
     app.logger.setLevel(logging.DEBUG)
     app.logger.info('Application startup')
