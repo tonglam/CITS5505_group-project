9e94d46c4dcf3a9a789e226bbd80b10e0cfcede6
enoch2022new <100208118+enoch2022new@users.noreply.github.com>
2024-04-06 14:44:04 +0800

    Change the .gitignore + running issue

--- app/logs.py
+++ app/logs.py
@@ -13,7 +13,7 @@ def configure_logging(app):
     file_handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.DEBUG)
-    app.logger.addHandler(file_handler``)
+    app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.DEBUG)
     app.logger.info('Application startup')
