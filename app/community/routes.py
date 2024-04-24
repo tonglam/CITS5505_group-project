df14d82ef9a525b14392bad6e60cceda78a8cc06
JiasenNiu <maxqq1005@gamil.com>
2024-04-24 11:39:47 +0800

    fix bug

--- app/community/routes.py
+++ app/community/routes.py
@@ -1,6 +1,6 @@
 """This module contains the routes for the community blueprint."""
 
-from flask import render_template
+from flask import render_template, url_for
 from flask_login import login_required
 
 from app.community import community_bp
@@ -11,8 +11,8 @@ def community():
     """Render the community page."""
     return render_template("community.html")
 
-@community_bp.route("/createCard")
+@community_bp.route("/createCommunity")
 @login_required
 def create():
     """Render the create page."""
-    return render_template("createCard.html")
+    return render_template("createCommunity.html")
