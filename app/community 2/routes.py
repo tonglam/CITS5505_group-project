6e1b1224dfad54cc12e6dd1c626867be91c959b4
JiasenNiu <maxqq1005@gamil.com>
2024-04-27 22:24:16 +0800

    merge css and fix bug

--- app/community 2/routes.py
+++ app/community 2/routes.py
@@ -1,6 +1,6 @@
 """This module contains the routes for the community blueprint."""
 
-from flask import render_template
+from flask import render_template, url_for
 from flask_login import login_required
 
 from app.community import community_bp
