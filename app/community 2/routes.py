89d5a0bcd0fd8936e3a7e7a572c22484b63fba82
JiasenNiu <maxqq1005@gamil.com>
2024-04-28 22:21:27 +0800

    Responsive design

---
+++ app/community 2/routes.py
@@ -0,0 +1,18 @@
+"""This module contains the routes for the community blueprint."""
+
+from flask import render_template
+from flask_login import login_required
+
+from app.community import community_bp
+
+@community_bp.route("/")
+@login_required
+def community():
+    """Render the community page."""
+    return render_template("community.html")
+
+@community_bp.route("/createCommunity")
+@login_required
+def create():
+    """Render the create page."""
+    return render_template("createCommunity.html")
