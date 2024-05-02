ccb10a5f685d49c769228a2f363032ec14778b2b
hayeen <gracehayeens@gmail.com>
2024-05-02 17:50:01 +0800

    backend - function implement

--- app/user/routes.py
+++ app/user/routes.py
@@ -3,6 +3,8 @@
 from flask import render_template
 from flask_login import login_required
 
+from app.extensions import db
+from app.models.request import Request
 from app.user import user_bp
 
 
@@ -10,4 +12,8 @@ from app.user import user_bp
 @login_required
 def user():
     """Render the user page."""
-    return render_template("user.html")
+
+    requests = db.session.query(Request).all()
+    items = [{"id": request.id, "title": request.title} for request in requests]
+    print("items: ", items)
+    return render_template("user.html", items=items)
