0f6481fef8ebe38c3736fb0e3362921f448f9204
tong <bluedragon00000@gmail.com>
2024-05-05 16:18:41 +0800

    Added Community.creator, changed migrations and test seeds.

--- app/community/routes.py
+++ app/community/routes.py
@@ -1,24 +1,28 @@
 """This module contains the routes for the community blueprint."""
+
 import logging
 import sys
-from flask import render_template, url_for,request,redirect,Flask
-from flask_login import login_required
-from app.extensions import db
-from app.models.community import Community
-from app.models.category import Category
-from flask_login import current_user
 
-from app.community import community_bp,forms
+from flask import Flask, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+
+from app.community import community_bp, forms
+from app.extensions import db
+from app.models.category import Category
+from app.models.community import Community
+
 logging.basicConfig(level=logging.DEBUG)
 # 创建日志处理程序，输出到标准输出流
 handler = logging.StreamHandler(sys.stdout)
+
 
 @community_bp.route("/")
 # @login_required
 def community():
     """Render the community page."""
     infolist = db.session.query(Community).all()
-    return render_template("community.html",infolist=infolist)
+    return render_template("community.html", infolist=infolist)
+
 
 @community_bp.route("/createCommunity")
 # @login_required
@@ -26,32 +30,34 @@ def create():
     """Render the create page."""
     form = forms.CreateForm(request.form)
     optionList = db.session.query(Category).all()
-    return render_template("createCommunity.html",optionList=optionList,form=form)
+    return render_template("createCommunity.html", optionList=optionList, form=form)
+
 
 @community_bp.route("/add_community", methods=["POST"])
 def add_community():
     form = forms.CreateForm(request.form)
     if form.validate_on_submit():
-        print('1111------------------------------321')
+        print("1111------------------------------321")
         print(form.name.data)
         print(form.description.data)
         print(form.category_id.data)
-        print('------------------------------')
-        community= Community (
+        print("------------------------------")
+        community = Community(
             name=form.name.data,
             description=form.description.data,
-            category_id=form.category_id.data
+            category_id=form.category_id.data,
         )
         db.session.add(community)
         db.session.commit()
         optionList = db.session.query(Category).all()
-        return render_template("createCommunity.html",optionList=optionList,form=form)
+        return render_template("createCommunity.html", optionList=optionList, form=form)
         # return redirect(url_for('community.create'))
     else:
-        print('error------------------------------error')
-        print('Form validation failed:', form.errors)  
-        return render_template("createCommunity.html",optionList=optionList,form=form)
+        print("error------------------------------error")
+        print("Form validation failed:", form.errors)
+        return render_template("createCommunity.html", optionList=optionList, form=form)
         # return redirect(url_for('community.create',form=form))
+
 
 @community_bp.route("/delete_community/<int:record_id>", methods=["GET", "DELETE"])
 def delete_community(record_id: int):
@@ -61,7 +67,7 @@ def delete_community(record_id: int):
         .first()
     )
     if record_entity is None:
-        return ''
+        return ""
 
     # if request.method == "GET":
     #     return ApiResponse(data={"record": record_entity.to_dict()}).json()
--- app/models/community.py
+++ app/models/community.py
@@ -14,20 +14,28 @@ class Community(db.Model):
     category_id: int = db.Column(db.Integer, db.ForeignKey("category.id"))
     description: str = db.Column(db.String(500))
     avatar_url: str = db.Column(db.String(300))
+    creator_id: str = db.Column(db.String(36), db.ForeignKey("user.id"))
     create_at: datetime = db.Column(db.DateTime, default=generate_time())
     update_at: datetime = db.Column(
         db.DateTime, default=generate_time(), onupdate=generate_time()
     )
 
     category = db.relationship("Category", backref=db.backref("communities", lazy=True))
+    creator = db.relationship("User", backref=db.backref("communities", lazy=True))
 
     def __init__(
-        self, name: str, category_id: int, description: str = "", avatar_url: str = ""
+        self,
+        name: str,
+        category_id: int,
+        description: str = "",
+        avatar_url: str = "",
+        creator_id: str = "",
     ) -> None:
         self.name = name
         self.category_id = category_id
         self.description = description
         self.avatar_url = avatar_url
+        self.creator_id = creator_id
 
     def __repr__(self) -> str:
         """Return a string representation of the community."""
@@ -44,6 +52,7 @@ class Community(db.Model):
             "category_id": self.category_id,
             "description": self.description,
             "avatar_url": self.avatar_url,
+            "creator_id": self.creator_id,
             "create_at": self.create_at,
             "update_at": self.update_at,
         }
