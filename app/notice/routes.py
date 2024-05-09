dd090173b2c5cee228ae616cc38e97dc8a8eb8d0
tong <bluedragon00000@gmail.com>
2024-05-09 15:35:46 +0800

    Added notice page.

--- app/notice/routes.py
+++ app/notice/routes.py
@@ -3,6 +3,7 @@
 from flask import render_template
 from flask_login import login_required
 
+from app.api.service import users_notices_service
 from app.notice import notice_bp
 
 
@@ -10,5 +11,8 @@ from app.notice import notice_bp
 @login_required
 def notice():
     """Render the notice page."""
+
+    notice = users_notices_service().get_json().get("data").get("user_notices")
+    print("notice: ", notice)
 
     return render_template("notice.html")
