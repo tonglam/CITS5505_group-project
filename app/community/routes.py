f122eedebacf3468f664a4ec175cd2bd3e922dfa
tong <bluedragon00000@gmail.com>
2024-05-14 01:01:22 +0800

    Community adjustments.

--- app/community/routes.py
+++ app/community/routes.py
@@ -1,23 +1,24 @@
 """This module contains the routes for the community blueprint."""
 
-from flask import render_template, request, jsonify
-from flask_login import current_user,login_required
+from flask import jsonify, render_template, request
+from flask_login import current_user, login_required
 
+from app.api.service import communities_service
 from app.community import community_bp, forms
 from app.extensions import db
 from app.models.category import Category
 from app.models.community import Community
 
 
-
-
 @community_bp.route("/")
 @login_required
 def community():
     """Render the community page."""
-    infolist = db.session.query(Community).all()
-    return render_template("community.html", infolist=infolist)
-@community_bp.route("/editCommunity/<int:community_id>")
+    communities = communities_service().get_json().get("data").get("communities")
+    return render_template("community.html", communities=communities)
+
+
+@community_bp.route("/edit/<int:community_id>")
 @login_required
 def edit(community_id: int):
     """Render the create page."""
@@ -28,17 +29,24 @@ def edit(community_id: int):
         .first()
     )
     option_list = db.session.query(Category).all()
-    return render_template("createCommunity.html", optionList=option_list,record_entity=record_entity, form=form)
+    return render_template(
+        "createCommunity.html",
+        optionList=option_list,
+        record_entity=record_entity,
+        form=form,
+    )
 
 
-@community_bp.route("/add_community", methods=["POST","GET"])
+@community_bp.route("/add_community", methods=["POST", "GET"])
 @login_required
 def add_community():
     """Render the create page. or do add with community"""
     if request.method == "GET":
         form = forms.CreateForm(request.form)
         option_list = db.session.query(Category).all()
-        return render_template("createCommunity.html", optionList=option_list, form=form)
+        return render_template(
+            "createCommunity.html", optionList=option_list, form=form
+        )
     if request.method == "POST":
         form = forms.CreateForm(request.form)
         if form.validate_on_submit():
@@ -50,14 +58,17 @@ def add_community():
             )
             db.session.add(new_community)
             db.session.commit()
-            return jsonify({"message": "Create success","ok":"ok","id":community.id}), 200
+            return (
+                jsonify({"message": "Create success", "ok": "ok", "id": community.id}),
+                200,
+            )
         # If you get here, it means the verification has not passed.
-        message=""
+        message = ""
         for field, errors in form.errors.items():
             for error in errors:
-                message+=f"{field.capitalize()}: {error}"
-        return jsonify({"message": message,"ok":"notok"}), 200
-    return jsonify({"message": "method not support","ok":"notok"}), 200
+                message += f"{field.capitalize()}: {error}"
+        return jsonify({"message": message, "ok": "notok"}), 200
+    return jsonify({"message": "method not support", "ok": "notok"}), 200
 
 
 @community_bp.route("/update_community/<int:community_id>", methods=["POST", "DELETE"])
@@ -70,11 +81,11 @@ def update_community(community_id: int):
         .first()
     )
     if record_entity is None:
-        return jsonify({"message": "Record not found","ok":"notok"}), 200
+        return jsonify({"message": "Record not found", "ok": "notok"}), 200
     if request.method == "DELETE":
         db.session.delete(record_entity)
         db.session.commit()
-        return jsonify({"message": "Community deleted successfully","ok":"ok"}), 200
+        return jsonify({"message": "Community deleted successfully", "ok": "ok"}), 200
     if request.method == "POST":
         form = forms.CreateForm(request.form)
         if form.validate_on_submit():
@@ -83,11 +94,14 @@ def update_community(community_id: int):
             record_entity.category_id = form.category_id.data
             db.session.commit()  # Submit changes to the database
             # After successful update, redirect to edit page
-            return jsonify({"message": "Edit success","ok":"ok","id":community_id}), 200
+            return (
+                jsonify({"message": "Edit success", "ok": "ok", "id": community_id}),
+                200,
+            )
         # If you get here, it means the verification has not passed.
-        message=""
+        message = ""
         for field, errors in form.errors.items():
             for error in errors:
-                message+=f"{field.capitalize()}: {error}"
-        return jsonify({"message": message,"ok":"notok","id":community_id}), 200
-    return jsonify({"message": "method not support","ok":"notok"}), 200
+                message += f"{field.capitalize()}: {error}"
+        return jsonify({"message": message, "ok": "notok", "id": community_id}), 200
+    return jsonify({"message": "method not support", "ok": "notok"}), 200
--- app/community/static/js/community.js
+++ app/community/static/js/community.js
@@ -1,20 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitBtn");
+  const nameInput = document.getElementById("name");
+  const descriptionInput = document.getElementById("description");
+  const categoryIdSelect = document.getElementById("category_id");
+  const csrf_token = document.getElementById("csrf_token");
+  // Listen for form submission events
+  submitBtn.addEventListener("click", async function (event) {
+    event.preventDefault(); // Prevent form from submitting by default
+    const { postFetch } = await import("../../../static/js/fetch.js");
+
+    // Manually collect form data
+
+    const postDataObj = {
+      name: nameInput.value,
+      description: descriptionInput.value,
+      category_id: categoryIdSelect.value,
+      csrf_token: csrf_token.value,
+    };
+    //Convert ordinary object to FormData instance
+    const postData = new FormData();
+    for (let key in postDataObj) {
+      postData.append(key, postDataObj[key]);
+    }
+    // Send POST request using postFetch
+    const response = await postFetch(
+      "{% if record_entity %}{{ url_for('community.update_community', community_id=record_entity.id) }}{% else %}{{ url_for('community.add_community') }}{% endif %}"
+    )(postData)({});
+    if (response.ok === "ok") {
+      // Handle successful responses such as redirects or prompts
+      alert(response.message);
+      window.location.href = "/communities/";
+    } else {
+      //Handling error responses
+      alert(response.message);
+    }
+  });
+});
 
 async function handDeleteCardClick(id) {
-    const { deleteFetch } = await import('../../../static/js/fetch.js');
-    const response = await deleteFetch(`/communities/update_community/${id}`)()()    
-    if (response.ok!=="ok") {
-        alert(response.message);
-    }else{
-        // page update
-        window.location.reload();
-    }
+  const { deleteFetch } = await import("../../../static/js/fetch.js");
+  const response = await deleteFetch(`/communities/update_community/${id}`)()();
+  if (response.ok !== "ok") {
+    alert(response.message);
+  } else {
+    // page update
+    window.location.reload();
+  }
 }
 function handEditCardClick(id) {
-    window.location.href = '/communities/editCommunity/' + id;
-}
-async function handInfoEditClick(element) {
-    const info = JSON.parse(element.getAttribute('data-info'));
-    const url = `/communities/update_community/${info.id}`;
-    const data = { info };
-    const response = await getFetch(url)(data)(); // The third parameter is header, if not, it will not be passed.
+  window.location.href = "/communities/editCommunity/" + id;
 }
