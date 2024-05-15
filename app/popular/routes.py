b7ab163933fef6d3a9caa182d88f37b1ca8f4b21
hayeen <gracehayeens@gmail.com>
2024-05-15 21:35:00 +0800

    popular page backend; popular page adjust

--- app/popular/routes.py
+++ app/popular/routes.py
@@ -3,11 +3,32 @@
 from flask import render_template
 from flask_login import login_required
 
-from app.popular import popular_bp
+from app.popular.service import (
+    top_community_service,
+    top_replied_service,
+    top_viewed_service,
+)
+
+from . import popular_bp
 
 
 @popular_bp.route("/")
 @login_required
 def popular():
     """Render the popular page."""
-    return render_template("popular.html")
+
+    # top replied data
+    top_replied = top_replied_service()
+
+    # top viewed data
+    top_viewed = top_viewed_service()
+
+    # top community data
+    top_community = top_community_service()
+
+    return render_template(
+        "popular.html",
+        topReplies=top_replied,
+        topViews=top_viewed,
+        topCommunities=top_community,
+    )
---
+++ app/popular/service.py
@@ -0,0 +1,84 @@
+"""This module contains the service functions for the popular blueprint."""
+
+from sqlalchemy import func
+
+from app.extensions import db
+from app.models import Category, Reply, Request, UserRecord
+
+
+def top_replied_service():
+    """Return the top replied requests."""
+
+    # pylint: disable=not-callable
+    top_replied = (
+        db.session.query(Reply)
+        .group_by(Reply.request_id)
+        .order_by(func.count(Reply.request_id))
+        .limit(5)
+        .all()
+    )
+
+    top_replied_info = [
+        {
+            "id": reply.request.id,
+            "username": reply.request.author.username,
+            "title": reply.request.title,
+        }
+        for reply in top_replied
+    ]
+
+    return top_replied_info
+
+
+def top_viewed_service():
+    """Return the top viewed requests."""
+
+    # pylint: disable=not-callable
+    top_viewed = (
+        db.session.query(UserRecord)
+        .group_by(UserRecord.request_id)
+        .order_by(func.count(UserRecord.request_id))
+        .limit(5)
+        .all()
+    )
+
+    top_viewed_info = [
+        {
+            "id": record.request.id,
+            "username": record.request.author.username,
+            "title": record.request.title,
+        }
+        for record in top_viewed
+    ]
+
+    return top_viewed_info
+
+
+def top_community_service():
+    """Return the top communities"""
+
+    # pylint: disable=not-callable
+    top_community = (
+        db.session.query(Request)
+        .group_by(Request.community_id)
+        .order_by(func.count(Request.id))
+        .limit(4)
+        .all()
+    )
+
+    community_category = db.session.query(Category).all()
+    community_category_dict = {
+        category.id: category.name for category in community_category
+    }
+
+    top_community_info = [
+        {
+            "id": request.id,
+            "communityname": request.community.name,
+            "category": community_category_dict[request.community.category_id],
+            "description": request.community.description,
+        }
+        for request in top_community
+    ]
+
+    return top_community_info
