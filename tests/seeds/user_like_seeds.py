85c3fe17025764495daeff44a388189ea2ee51cf
enoch2022new <100208118+enoch2022new@users.noreply.github.com>
2024-05-19 01:44:52 +0800

    fix error / havent passed test

--- tests/seeds/user_like_seeds.py
+++ tests/seeds/user_like_seeds.py
@@ -4,6 +4,7 @@ import random
 
 from app.extensions import db
 from app.models.request import Request
+from app.models.reply import Reply
 from app.models.user import User
 from app.models.user_like import UserLike
 
@@ -15,11 +16,15 @@ def create_seed_user_like_data() -> list:
 
     users = [user.id for user in User.query.all()]
     requests = [request.id for request in Request.query.all()]
+    replies = [reply.id for reply in Reply.query.all()]
+
 
     return [
         {
             "user_id": random.choice(users),
             "request_id": random.choice(requests),
+            "reply_id": random.choice(replies),
+
         }
         for _ in range(100)
     ]
@@ -36,6 +41,7 @@ def seed_user_like():
         user_like = UserLike(
             user_id=data["user_id"],
             request_id=data["request_id"],
+            reply_id=data["reply_id"],
         )
         db.session.add(user_like)
 
--- tests/seeds/user_save_seeds.py
+++ tests/seeds/user_save_seeds.py
@@ -4,6 +4,7 @@ import random
 
 from app.extensions import db
 from app.models.request import Request
+from app.models.reply import Reply
 from app.models.user import User
 from app.models.user_save import UserSave
 
@@ -15,11 +16,13 @@ def create_seed_user_save_data() -> list:
 
     users = [user.id for user in User.query.all()]
     requests = [request.id for request in Request.query.all()]
+    replies = [reply.id for reply in Reply.query.all()]
 
     return [
         {
             "user_id": random.choice(users),
             "request_id": random.choice(requests),
+            "reply_id": random.choice(replies)
         }
         for _ in range(100)
     ]
@@ -36,6 +39,7 @@ def seed_user_save():
         user_save = UserSave(
             user_id=data["user_id"],
             request_id=data["request_id"],
+            reply_id=data["reply_id"]
         )
         db.session.add(user_save)
 
